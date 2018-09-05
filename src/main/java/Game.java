7ae65107a6cb44433f70e77fa3f4afa0c20f0237
labsolam <labsolam@users.noreply.github.com>
2018-09-05 22:41:38 +0100

    Can add tiles after a key has been pressed. Added reset function, but the last tile to add to the board before the game ends doesn't appear for an unknow reason

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,43 +1,72 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Game extends Application {
 
-    public Tile[] board;
-    int score;
+    private Tile[] board;
+    private int score;
+    private Stage stage;
+
+    private EventHandler handle = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            switch(event.getCode()){
+                case UP:
+                    moveUp();
+                    event.consume();
+                    break;
+                case DOWN:
+                    moveDown();
+                    event.consume();
+                    break;
+                case LEFT:
+                    moveLeft();
+                    event.consume();
+                    break;
+                case RIGHT:
+                    moveRight();
+                    event.consume();
+                    break;
+            }
+        }
+    };
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Display board
-        //Add tiles
-        createGame();
+        stage = primaryStage;
+        startGame();
+    }
 
+    private void startGame(){
+        createBoard();
+        score = 0;
         TilePane tilePane = new TilePane();
-        //tilePane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 
         for(Tile tile : board){
             tilePane.getChildren().add(new StackPane(tile, tile.getScoreText()));
         }
 
         Scene scene = new Scene(tilePane,400,400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, handle);
+
+        stage.setMaxHeight(400);
+        stage.setMaxWidth(400);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    private void createGame(){
-        board = new Tile[15];
+    private void createBoard(){
+        board = new Tile[16];
 
         for(int i = 0; i < board.length; i++){
             board[i] = new Tile();
@@ -65,4 +94,50 @@ public class Game extends Application {
         return 4;
     }
 
+    private void moveUp(){
+        System.out.println("Up");
+        addTile();
+
+        if(checkGameOver()){
+            gameOver();
+            System.out.println("RANNNN");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            resetGame();
+        }
+    }
+
+    private void moveDown(){
+        System.out.println("Down");
+        addTile();
+    }
+
+    private void moveLeft(){
+        System.out.println("Left");
+        addTile();
+    }
+
+    private void moveRight(){
+        System.out.println("Right");
+        addTile();
+    }
+
+    private boolean checkGameOver(){
+        for(Tile tile : board){
+            if(tile.getScore() == 0) return false;
+        }
+        return true;
+    }
+
+    private void resetGame(){
+        startGame();
+    }
+
+    private void gameOver(){
+        stage.getScene().removeEventFilter(KeyEvent.KEY_PRESSED, handle);
+        System.out.println("GAME OOOOOOOVVVVVVVVEEEEEEERRRRR");
+    }
 }
